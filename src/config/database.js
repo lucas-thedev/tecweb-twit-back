f362669259f279bf4e422079e852198b2238d251
bernardo.mafra <bernardo.mafra@mundiale.com.br>
2021-04-17 19:02:08 -0300

    chore: add initial structure

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'tecweb_twitt_root',
+  password: 'tecweb_twitt_root',
+  database: 'twitt',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
